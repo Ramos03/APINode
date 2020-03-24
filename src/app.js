36ba32462b2589c85aa48273455f95077f0acbd2
Vinicius Ramos <vinicius.d.ramos@hotmail.com>
2020-03-24 00:41:36 -0300

    Primeiro commit

---
+++ src/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+const app = express();
+const router = express.Router();
+
+//Connecta ao banco
+mongoose.connect("mongodb+srv://vinicius:Vinicius32@apinode-u50zg.mongodb.net/test?retryWrites=true&w=majority");
+
+// Carrega as models
+const Product = require('./models/product');
+
+// Carrega as rotas
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', indexRoute);
+app.use('/products', productRoute);
+
+
+module.exports = app;
---
+++ src/controllers/product-controller.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Product = mongoose.model('Product');
+
+exports.get = (req, res, next) =>{
+    Product
+        .find({
+            active: true
+        }, 'title price slug')
+        .then(data=> {
+            res.status(200).send(data);
+        }).catch(e =>{
+            res.status(400).send(e);
+        });
+}
+
+exports.post = ('/', (req, res, next) => {
+    let product = new Product(req.body);
+    product
+        .save()
+        .then(x=>{
+            res.status(201).send({
+                message: "Produto cadastrado com sucesso!"
+            });
+        }).catch(e =>{
+            res.status(400).send({
+                message: "Falha ao cadastrar o produto",
+                data: e
+            });
+        });
+
+});
+
+exports.put = ('/:id', (req, res, next) => {
+    let id = req.params.id;
+    res.status(201).send({ id: id, item: req.body });
+});
+
+exports.delete = ('/:id', (req, res, next) => {
+    res.status(200).send(req.body);;
+});
